2cfa12e7430d1ae1e317ec998af8ca106c8fd26d
zhangnq <admin@nbhao.org>
2018-01-16 11:25:19 +0800

    修复数据库没在运行不报警的问题

--- check_pglag.sh
+++ check_pglag.sh
@@ -11,6 +11,11 @@ lag_c=10800
 #lag time second
 lag_tm=`/opt/PostgreSQL/93/bin/psql -U postgres -p 5432 -d postgres -A -t -c "SELECT CASE WHEN pg_last_xlog_receive_location() = pg_last_xlog_replay_location() THEN 0 ELSE EXTRACT (EPOCH FROM now() - pg_last_xact_replay_timestamp())::integer END AS replication_lag;"`
 
+if [ $? -ne 0 ];then
+  echo "CRITICAL - PostgreSQL is not running."
+  exit $STATE_CRITICAL
+fi
+
 if [ $lag_tm -gt $lag_w ];then
   echo "WARNING - Lag time $lag_tm s. | lag_tm=$lag_tm;$lag_w;$lag_c;;"
   exit $STATE_WARNING
